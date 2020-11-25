1f642c425f297280e8dc74305367fd984da3b606
KarthikaVignesh <karthikathirumalaiappan@gmail.com>
2020-11-25 09:57:50 +0530

    color changed

--- sketch.js
+++ sketch.js
@@ -11,8 +11,8 @@ function draw() {
   mr.x=World.mouseX;
   mr.y=World.mouseY;
   if(mr.x-fr.x<mr.width/2+fr.width/2&&fr.x-mr.x<mr.width/2+fr.width/2&&mr.y-fr.y<mr.height/2+fr.height/2&&fr.y-mr.y<mr.height/2+fr.height/2){
-    mr.shapeColor="blue";
-    fr.shapeColor="blue";
+    mr.shapeColor="red";
+    fr.shapeColor="red";
   }
   else{
     mr.shapeColor="green";
